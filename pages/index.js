ebd82b0f4a7be48e9eafc86aca7d8dccdbbddbb7
Emanuel-js <etioaman1960@gmail.com>
2021-12-19 18:20:47 +0300

    index page its contain main function of the project

--- pages/index.js
+++ pages/index.js
@@ -1,70 +1,113 @@
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
-import { useState } from 'react';
+import { useState,useEffect,useContext} from 'react';
 import styled from 'styled-components';
 import Card from '../components/Card';
-
 
 const Container = styled.div`
 display: grid;
 grid-template-columns: auto auto auto;
 margin-top:40px;
 `;
+export default function Home() {
 
-export default function Home(results) {
-  const intialState = results;
-  const [characters, setCharacters] = useState(intialState.characters);
-  // console.log(characters);
+  const [characters, setCharacters] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getData = async() => {
+      const client = new ApolloClient({
+        uri: "https://rickandmortyapi.com/graphql/",
+        cache: new InMemoryCache(),
+      });
+      const { data } = await client.query({
+        query: gql`
+          query {
+            characters(page: 1) {
+              info {
+                count
+                pages
+              }
+              results {
+                name
+                id
+                location {
+                  name
+                  id
+                }
+                image
+                origin {
+                  name
+                  id
+                }
+                episode {
+                  id
+                  episode
+                  air_date
+                }
+              }
+            }
+          }
+        `,
+      });
+      setCharacters(data.characters.results);
+      setLoading(false);
+    }
+    getData();
+  })
+  if (loading) {
+    return <div class="lds-hourglass"></div>
+  }
+
   return (
     <Container>
       {
         characters?.map(character =>
-          <Card key={character.id} character={character} />
+          <Card key={character.id}  character={character} />
           )
        }
     </Container>
   )
 }
 
+// export async function getServerSideProps() {
+//   const client = new ApolloClient({
+//     uri: "https://rickandmortyapi.com/graphql/",
+//     cache: new InMemoryCache(),
+//   });
+//   const { data } = await client.query({
+//     query: gql`
+//       query {
+//         characters(page: 1) {
+//           info {
+//             count
+//             pages
+//           }
+//           results {
+//             name
+//             id
+//             location {
+//               name
+//               id
+//             }
+//             image
+//             origin {
+//               name
+//               id
+//             }
+//             episode {
+//               id
+//               episode
+//               air_date
+//             }
+//           }
+//         }
+//       }
+//     `,
+//   });
 
-export async function getStaticProps() {
-  const client = new ApolloClient({
-    uri: "https://rickandmortyapi.com/graphql/",
-    cache: new InMemoryCache(),
-  });
-  const { data } = await client.query({
-    query: gql`
-      query {
-        characters(page: 1) {
-          info {
-            count
-            pages
-          }
-          results {
-            name
-            id
-            location {
-              name
-              id
-            }
-            image
-            origin {
-              name
-              id
-            }
-            episode {
-              id
-              episode
-              air_date
-            }
-          }
-        }
-      }
-    `,
-  });
-
-  return {
-    props: {
-      characters: data.characters.results,
-    },
-  };
-}
+//   return {
+//     props: {
+//       characters: data.characters.results,
+//     },
+//   };
+// }
