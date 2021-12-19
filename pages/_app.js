d4d7f54dcddff0e5c875aab273b93d6172dd8d53
Emanuel-js <etioaman1960@gmail.com>
2021-12-19 18:27:22 +0300

    letast update

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
-
+import {LikeProvider } from '../context/LikeContext';
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <LikeProvider>
       <Layout>
       <Component {...pageProps} />
     </Layout>
-    </>
+    </LikeProvider>
   )
 }
 
-export default MyApp
+export default MyApp;
