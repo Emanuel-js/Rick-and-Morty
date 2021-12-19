0c3640fbc6aa1578dbc7de7dee66b03c07645956
Emanuel-js <etioaman1960@gmail.com>
2021-12-19 19:20:47 +0300

    Api for searching characters

--- pages/api/characters.js
+++ pages/api/characters.js
@@ -18,6 +18,7 @@ export default async (req, res) => {
             results {
               name
               id
+              species
               location {
                 name
                 id
